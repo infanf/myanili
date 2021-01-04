fabf71205dba6aa23fda2ffef4809a87c5a21a01
Christopher Schreiner <git@infanf.com>
2021-01-04 15:24:21 +0100

    feat: highlight active module

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { MalUser } from '@models/user';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -10,23 +11,36 @@ import { SettingsComponent } from '../settings/settings.component';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
-  constructor(public malService: MalService, private modal: NgbModal) {
-    this.loggedIn = 'loading';
-  }
-  loggedIn: string | false = 'loading';
-  user?: MalUser;
-
-  ngOnInit(): void {
+export class NavbarComponent {
+  constructor(public malService: MalService, private modal: NgbModal, private router: Router) {
     this.malService.loggedIn.subscribe(loggedIn => {
       this.loggedIn = loggedIn;
     });
     this.malService.user.subscribe(user => {
       this.user = user;
     });
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const path = event.urlAfterRedirects;
+        this.setModule(path);
+      }
+    });
   }
+  loggedIn: string | false = 'loading';
+  user?: MalUser;
+  module?: 'anime' | 'manga';
 
   showSettings() {
     this.modal.open(SettingsComponent);
+  }
+
+  setModule(path: string) {
+    if (path.includes('anime')) {
+      this.module = 'anime';
+    } else if (path.includes('manga')) {
+      this.module = 'manga';
+    } else {
+      delete this.module;
+    }
   }
 }
