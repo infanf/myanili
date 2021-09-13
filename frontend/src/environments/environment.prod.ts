a97d4b55533654afe6436f697be301596479fe01
Christopher Schreiner <git@infanf.com>
2021-09-13 19:20:20 +0200

    fix: missing url in prod env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   backend: 'https://mal.myani.li/',
+  jikanUrl: 'https://api.jikan.moe/v3/',
   jikanFallbackUrl: 'https://jikan.myani.li/v3/',
 };
