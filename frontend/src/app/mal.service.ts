0af33a3dd180642b8a46795648acd664d71e6e8d
Christopher Schreiner <git@infanf.com>
2022-02-04 16:27:57 +0100

    fix: related manga and anime from jikan not working

--- frontend/src/app/mal.service.ts
+++ frontend/src/app/mal.service.ts
@@ -6,6 +6,8 @@ import { MalUser, UserResponse } from '@models/user';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+import { GlobalService } from './global.service';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -14,7 +16,7 @@ export class MalService {
   private isLoggedIn = new BehaviorSubject<string | false>('***loading***');
   private malUser = new BehaviorSubject<MalUser | undefined>(undefined);
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private glob: GlobalService) {
     const malUser = JSON.parse(localStorage.getItem('malUser') || 'false') as MalUser | false;
     if (malUser) {
       this.isLoggedIn.next(malUser.name);
@@ -66,25 +68,24 @@ export class MalService {
     });
   }
 
-  async getJikanData<T>(url: string, v3 = false): Promise<T> {
+  async getJikanData<T>(url: string, v3 = false, retry = false): Promise<T> {
     try {
-      const response = await new Promise((r, rj) => {
-        this.httpClient
-          .get<T>(`${v3 ? environment.jikan3Url : environment.jikanUrl}${url}`)
-          .subscribe(r, rj);
-      });
-      return response as T;
+      const response = await fetch(`${v3 ? environment.jikan3Url : environment.jikanUrl}${url}`);
+      if (response.status === 429 && !retry) {
+        await this.glob.sleep(1000);
+        return this.getJikanData<T>(url, v3, true);
+      }
+      return response.json() as unknown as T;
     } catch (e) {
-      return new Promise((r, rj) => {
-        this.httpClient
-          .get<T>(`${v3 ? environment.jikan3FallbackUrl : environment.jikanFallbackUrl}${url}`)
-          .subscribe(r, rj);
-      });
+      const response = await fetch(
+        `${v3 ? environment.jikan3FallbackUrl : environment.jikanFallbackUrl}${url}`,
+      );
+      return response.json() as unknown as T;
     }
   }
 
   async getJikan(type: 'anime' | 'manga', id: number): Promise<JikanInstance> {
-    return this.getJikanData<JikanInstance>(`${type}/${id}`);
+    return this.getJikanData<JikanInstance>(`${type}/${id}`, true);
   }
 
   async checkLogin() {
