fba0550347a6975e90121cb3df1baa384c944c29
Christopher Schreiner <git@infanf.com>
2022-08-28 12:24:42 +0200

    feat(#49): implement login

--- frontend/src/app/manga/mangaupdates.service.ts
+++ frontend/src/app/manga/mangaupdates.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BakaUser } from '@models/baka';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -8,20 +9,22 @@ import { environment } from 'src/environments/environment';
 export class MangaupdatesService {
   private baseUrl = environment.backend + 'baka/v1/';
   private accessToken = '';
-  private userSubject = new BehaviorSubject<string>('');
-  loggedIn = false;
+  private userSubject = new BehaviorSubject<BakaUser | undefined>(undefined);
 
   constructor() {
     this.accessToken = String(localStorage.getItem('bakaAccessToken'));
     if (this.accessToken && this.accessToken !== 'null') {
       this.login().then(user => {
-        this.userSubject.next(user || '');
+        this.userSubject.next(user);
       });
     }
-    this.login('test', 'test').then(console.log);
   }
 
-  async login(username?: string, password?: string, saveLogin = false): Promise<string | false> {
+  async login(
+    username?: string,
+    password?: string,
+    saveLogin = false,
+  ): Promise<BakaUser | undefined> {
     if (!username || !password) {
       return this.checkLogin();
     }
@@ -32,18 +35,32 @@ export class MangaupdatesService {
       }),
       body: JSON.stringify({ username, password }),
     });
-    if (!result.ok) return false;
-    return '';
+    if (!result.ok) return undefined;
+    const response = (await result.json()) as { context: { session_token: string } };
+    this.accessToken = response.context.session_token;
+    localStorage.setItem('bakaAccessToken', this.accessToken);
+    return this.checkLogin();
   }
 
-  async checkLogin(): Promise<string | false> {
-    if (!this.accessToken) return false;
+  async checkLogin(): Promise<BakaUser | undefined> {
+    if (!this.accessToken) return undefined;
     const result = await fetch(`${this.baseUrl}account/profile`, {
       headers: new Headers({
         Authorization: `Bearer ${this.accessToken}`,
       }),
     });
-    if (!result.ok) return false;
-    return '';
+    if (!result.ok) return undefined;
+    const response = (await result.json()) as BakaUser;
+    return response;
+  }
+
+  logoff() {
+    this.accessToken = '';
+    this.userSubject.next(undefined);
+    localStorage.removeItem('bakaAccessToken');
+  }
+
+  get user() {
+    return this.userSubject.asObservable();
   }
 }
---
+++ frontend/src/app/models/baka.ts
@@ -0,0 +1,4 @@
+export interface BakaUser {
+  url: string;
+  username: string;
+}
