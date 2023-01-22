f00ac262170d281153b544d1e0f34ceea49bf316
Christopher Schreiner <git@infanf.com>
2023-01-22 13:38:11 +0100

    fix: dark theme notifications

--- frontend/src/app/navbar/navbar.module.ts
+++ frontend/src/app/navbar/navbar.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DirectivesModule } from '@app/directives/directives.module';
 import { ComponentsModule } from '@components/components.module';
 import { IconModule } from '@icon/icon.module';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
@@ -11,7 +12,14 @@ import { NavbarTopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [NavbarBottomComponent, NavbarTopComponent, NotificationsComponent],
-  imports: [CommonModule, NgbDropdownModule, IconModule, ComponentsModule, RouterModule],
+  imports: [
+    CommonModule,
+    NgbDropdownModule,
+    IconModule,
+    ComponentsModule,
+    DirectivesModule,
+    RouterModule,
+  ],
   exports: [NavbarBottomComponent, NavbarTopComponent, NotificationsComponent],
 })
 export class NavbarModule {}
