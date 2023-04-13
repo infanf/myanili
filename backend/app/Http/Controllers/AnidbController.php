8fd78604c10b49c42188002edd7b7c13bc0b0f75
Christopher Schreiner <git@infanf.com>
2023-04-13 11:38:17 +0200

    chore: add anidb cache

--- backend/app/Http/Controllers/AnidbController.php
+++ backend/app/Http/Controllers/AnidbController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -15,12 +14,15 @@ class AnidbController extends Controller
      */
     public function __construct()
     {
-        //
+        $this->initCache();
     }
+
+    private $cache;
 
     private static $baseUrl = "http://api.anidb.net:9001/";
 
-    public function redirect(Request $request) {
+    public function redirect(Request $request)
+    {
         $url = preg_replace('/^anidb\//', self::$baseUrl, $request->path());
         $auth = $request->header('Authorization');
         $body = $request->getContent();
@@ -34,6 +36,12 @@ class AnidbController extends Controller
         if ($params) {
             $url .= "?" . http_build_query($params);
         }
+        $response = $this->getCache($url);
+        if ($response) {
+            return (new Response($response, 200))
+                ->header('Content-Type', 'application/xml')
+                ->header('X-MyAniLi-From-Cache', 'true');
+        }
         $ch = curl_init($url);
         curl_setopt_array($ch, [
             CURLOPT_RETURNTRANSFER => true,
@@ -46,7 +54,52 @@ class AnidbController extends Controller
         }
         $response = curl_exec($ch);
         curl_close($ch);
-        return (new Response($response, curl_getinfo($ch, CURLINFO_HTTP_CODE) ))
-        ->header('Content-Type', curl_getinfo($ch, CURLINFO_CONTENT_TYPE));
+        $this->setCache($url, $response);
+        return (new Response($response, curl_getinfo($ch, CURLINFO_HTTP_CODE)))
+            ->header('Content-Type', curl_getinfo($ch, CURLINFO_CONTENT_TYPE));
+    }
+
+    private function initCache()
+    {
+        // create sqlite cache if not exists
+        $this->cache = new \SQLite3('anidb_cache.sqlite');
+        $this->cache->exec("CREATE TABLE IF NOT EXISTS cache (url VARCHAR(32) PRIMARY KEY, data TEXT, timestamp INTEGER)");
+    }
+
+    /**
+     * @param string $url
+     * @param string $data
+     * @throws \Exception
+     * @return void
+     */
+    private function setCache($url, $data)
+    {
+        if (!$this->cache) {
+            $this->initCache();
+        }
+        $url = md5($url);
+        $timestamp = time();
+        $data = base64_encode($data);
+        $stmt = $this->cache->prepare("INSERT OR REPLACE INTO cache (url, data, timestamp) VALUES (:url, :data, :timestamp)");
+        $stmt->bindValue(':url', $url);
+        $stmt->bindValue(':data', $data);
+        $stmt->bindValue(':timestamp', $timestamp);
+        $stmt->execute();
+    }
+
+    private function getCache($url)
+    {
+        if (!$this->cache) {
+            $this->initCache();
+        }
+        $url = md5($url);
+        $stmt = $this->cache->prepare("SELECT data, timestamp FROM cache WHERE url = :url");
+        $stmt->bindValue(':url', $url);
+        $result = $stmt->execute();
+        $row = $result->fetchArray(SQLITE3_ASSOC);
+        if ($row && $row['timestamp'] > time() - 60 * 60 * 24) {
+            return base64_decode($row['data']);
+        }
+        return null;
     }
 }
