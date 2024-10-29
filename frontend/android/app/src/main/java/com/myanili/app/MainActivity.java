8fe8081e3dcd940074b2f26166d809df77a61229
Christopher Schreiner <cs@elunic.com>
2024-10-29 11:41:47 +0100

    chore: initialize capacitor

---
+++ frontend/android/app/src/main/java/com/myanili/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.myanili.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myanili.app',
+  appName: 'myanili',
+  webDir: 'dist/frontend'
+};
+
+export default config;
