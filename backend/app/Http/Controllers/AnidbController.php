b73055f0f3975ead1a4eb974087fdc59a302bd63
Christopher Schreiner <git@infanf.com>
2025-06-26 23:11:38 +0200

    Revert "feat: reenable anidb"
    This reverts commit 69db4ff302f7102bd4f1811547bc68e1cac767e2.

--- backend/app/Http/Controllers/AnidbController.php
+++ backend/app/Http/Controllers/AnidbController.php
@@ -24,7 +24,7 @@ class AnidbController extends Controller
     public function redirect(Request $request)
     {
         // doesn't work on production due to rate limiting
-        // return [];
+        return [];
         $url = preg_replace('/^anidb\//', self::$baseUrl, $request->path());
         $auth = $request->header('Authorization');
         $body = $request->getContent();
