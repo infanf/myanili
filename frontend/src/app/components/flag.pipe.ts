4d5798c94d29908cbc483161d6c516c5936e9bbf
Christopher Schreiner <cs@elunic.com>
2025-09-04 17:29:47 +0200

    feat: update Angular dependencies

--- frontend/src/app/components/flag.pipe.ts
+++ frontend/src/app/components/flag.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import flag from 'country-code-emoji';
+// @ts-ignore
+import { countryCodeEmoji } from 'country-code-emoji';
 
 @Pipe({
   name: 'flag',
@@ -8,7 +9,7 @@ import flag from 'country-code-emoji';
 export class FlagPipe implements PipeTransform {
   transform(value?: string, hideUnknown = false): string {
     if (!value) value = 'xx';
-    const emoji = flag(value);
-    return flag('xx') === emoji && hideUnknown ? '' : emoji;
+    const emoji = countryCodeEmoji(value);
+    return countryCodeEmoji('xx') === emoji && hideUnknown ? '' : emoji;
   }
 }
