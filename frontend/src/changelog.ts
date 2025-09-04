6e0a0adc57db018452a015f73e209d4355055e59
Christopher Schreiner <cs@elunic.com>
2025-09-04 17:16:45 +0200

    chore: version bump

--- frontend/src/changelog.ts
+++ frontend/src/changelog.ts
@@ -1,11 +1,14 @@
 export const changelog: Changelog = {
   changes: [
     {
+      version: '2.33.9',
+      date: new Date('2025-09-04'),
+      fixes: ['Login: need reload to show user for MAL and Livechart'],
+    },
+    {
       version: '2.33.8',
       date: new Date('2025-06-26'),
-      fixes: [
-        'Anisearch: fix OAuth authentication by implementing PKCE support',
-      ],
+      fixes: ['Anisearch: fix OAuth authentication by implementing PKCE support'],
     },
     {
       version: '2.33.7',
